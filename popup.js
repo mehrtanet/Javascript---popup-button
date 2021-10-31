1f9be7786d3b281a287f2cfd6d242e2a512ab984
Fariba.Mshokoh <faribamohamadi.mr@gmail.com>
2021-10-31 18:05:17 +0330

    commit all

---
+++ popup.js
@@ -0,0 +1,27 @@
+// popup button
+let popupBtns = document.querySelectorAll("button.popup-btn")
+if (popupBtns) {
+    // we used of var because we use that twice (public variable)
+    var popupWrapper = document.createElement("div")
+    popupWrapper.className = "popup-wrapper"
+    document.body.prepend(popupWrapper)
+}
+
+popupBtns.forEach(popupBtn => {
+    popupBtn.addEventListener("click", e => {
+        let popup = popupBtn.nextElementSibling
+        popup.classList.add("show")
+        popupWrapper.classList.add("show")
+
+        // close button
+        let popupCloseFunc = e => {
+            popup.classList.remove("show")
+            popupWrapper.classList.remove("show")
+        }
+        let popupCloses = document.querySelectorAll(".popup-close")
+        popupCloses.forEach(popupClose => {
+            popupClose.addEventListener("click", popupCloseFunc)
+        })
+        popupWrapper.addEventListener("click", popupCloseFunc)
+    })
+})
